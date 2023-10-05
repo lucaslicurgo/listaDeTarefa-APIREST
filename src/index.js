234cd4eb90a50fd58224e247cfeb5af863d9523c
Lucas Licurgo <lucasclicurgo@gmail.com>
2023-10-05 11:21:39 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const rotas = require('./rotas');
+
+const app = express();
+
+app.use(express.json());
+app.use(rotas);
+
+const port = process.env.PORT || 3000
+
+app.listen(port);
+
